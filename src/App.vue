7bd81ed714364ea02108f3b02f530f6f9fad643e
sofanisba <stefanov.tamara@gmail.com>
2017-05-05 00:34:45 -0400

    setup

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>XDA FEED</h1>
+    </header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+#app {
+}
+
+header {
+  height: 6rem;
+  background-color: green;
+  padding: 2rem;
+  margin: 0px;
+  align-items: center;
+}
+</style>
---
+++ src/components/urlInput.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div id="enter-url">
+    <input type="text" placeholder="URL" v-model="url">
+    {{url}}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      url: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#enter-url {
+  padding: 2rem;
+}
+</style>
