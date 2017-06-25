6458a3d2c159c13b9354c0e8cfa02b81cbebb6fa
sofanisba <stefanov.tamara@gmail.com>
2017-06-25 19:43:48 -0400

    reset errors on each navigation

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
 
+import App from './App'
 import store from './store/store'
-import router from './router'
+import { routes } from './router'
 import './styles/styles.scss'
 import { sync } from 'vuex-router-sync'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: routes
+})
+
+router.beforeEach((to, from, next) => {
+  store.commit('SET_ERRORS', {})
+  next()
+})
 
 sync(store, router)
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,43 @@
-import Vue from 'vue'
-import Router from 'vue-router'
 import getFeed from '../components/getFeed'
 import urlInput from '../components/urlInput'
 import addItem from '../components/addItem'
 import itemDetails from '../components/itemDetails'
 import thanks from '../components/thanks'
 
-Vue.use(Router)
+import store from '../store/store'
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'get-feed',
-      component: getFeed
-    },
-    {
-      path: '/url',
-      name: 'url-input',
-      component: urlInput
-    },
-    {
-      path: '/add',
-      name: 'add-item',
-      component: addItem
-    },
-    {
-      path: '/:type/:id',
-      component: itemDetails,
-      props: true
-    },
-    {
-      path: '/thanks',
-      name: 'thanks',
-      component: thanks
+export const routes = [
+  {
+    path: '/',
+    name: 'get-feed',
+    component: getFeed
+  },
+  {
+    path: '/url',
+    name: 'url-input',
+    component: urlInput
+  },
+  {
+    path: '/add',
+    name: 'add-item',
+    component: addItem
+  },
+  {
+    path: '/:type/:id',
+    component: itemDetails,
+    props: true
+  },
+  {
+    path: '/thanks',
+    name: 'thanks',
+    component: thanks,
+    beforeEnter: (to, from, next) => {
+      if (Object.keys(store.getters.errors).length > 0) {
+        console.log('router errors true')
+        next(false)
+      } else {
+        console.log('router errors false')
+      }
     }
-  ]
-})
+  }
+]
