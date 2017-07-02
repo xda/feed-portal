65ecf33920878ed29e25b994f723890ea707df02
sofanisba <stefanov.tamara@gmail.com>
2017-07-02 14:52:30 -0400

    set base urls in environments

--- config/staging.env.js
+++ config/staging.env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  NODE_ENV: '"staging"'
+  NODE_ENV: '"staging"',
+  BASE_URL: '"https://feed-staging.xda-developers.com/"'
 }
--- src/store/api.js
+++ src/store/api.js
@@ -1,14 +1,7 @@
 import axios from 'axios'
 
-let baseURL
-if (process.env.NODE_ENV === 'production') {
-  baseURL = 'http://feed.xda-developers.com/'
-} else {
-  baseURL = 'http://feed-staging.xda-developers.com/'
-}
-
 const instance = axios.create({
-  baseURL: baseURL,
+  baseURL: process.env.BASE_URL,
   timeout: 6000,
   headers: {'Authorization': 'Bearer 4GDcWT0gqFoUqBHF8MtKEXgndls50b'}
 })
