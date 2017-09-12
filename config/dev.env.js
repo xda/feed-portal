3bb6dfa64dce06095a56cda6fe17f220d1361625
Jeff Corcoran <jcorcoran+github@gmail.com>
2017-09-12 15:07:26 -0400

    Update dev url, clientid and port

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,5 +2,7 @@ var merge = require('webpack-merge')
 var stagingEnv = require('./staging.env')
 
 module.exports = merge(stagingEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  BASE_URL: '"http://feed.xda-developers.local:8000"',
+  CLIENT_ID: '"XDA-FEED-USP-DEV"'
 })
