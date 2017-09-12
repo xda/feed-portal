51baea3a4787e7df263e15eca452cbbadc320108
Jeff Corcoran <jcorcoran+github@gmail.com>
2017-09-12 14:56:22 -0400

    Update hostname

--- config/docker.env.js
+++ config/docker.env.js
@@ -3,6 +3,6 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"docker"',
-  BASE_URL: '"http://localhost:8000"',
+  BASE_URL: '"http://feed.xda-developers.local:8000"',
   CLIENT_ID: '"XDA-FEED-USP-DEV"'
 })
